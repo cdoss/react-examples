1c432fbf625981779eeb034797109d4035b57b6c
Chandy <chandy.doss@gmail.com>
2017-12-19 11:15:53 -0500

    adding handler for adding items

--- todolist/src/ToDoList.js
+++ todolist/src/ToDoList.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import './index.css';
-import ToDoItem from './ToDoItem.js';
+import ToDoItem from './ToDoItem';
 
 class ToDoList extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+  }
+  
+  addItem(item) {
+    var itemsArray = this.state.items;
+
+    if (this._inputElement.value !== '') {
+      itemsArray.unshift({
+        text: this._inputElement,
+        key: Date.now()
+      });
+
+
+      this.setState({
+        items: itemsArray
+      });
+    }
+    this.inputElement = '';
+    console.log('To Do Items: ' + itemsArray);
+    item.preventDefault();
+  }
+  
   render() {
     return (
       <div>      
         <h2>To Do List</h2>
-        <div>
-          <input placeholder="Enter task"></input>
+        <form onSubmit={this.addItem}>
+          <input ref={ (a) => this._inputElement = a } placeholder="Enter task"></input>
           <button>Add</button>
-        </div>
+        </form>
         <div className="toDoList">
           <ToDoItem />
           <ToDoItem />
